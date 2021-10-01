5c17818451f28e24e9b515623692efc25ab09499
Imam Ramadhan <imamrmdn99@gmail.com>
2021-10-01 09:18:26 +0700

    update project

---
+++ Minggu 2/[A_A29_IMAM RAMADHAN].cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+struct sumOperation{
+	int a, b, index;;
+	int arrA[20], arrB[20], arrC[20];
+
+	string aString, bString;
+};
+
+string changeStr(int a){
+	string str;
+
+	stringstream obj;
+	obj << a;
+	obj >> str;
+
+	return str;
+}
+
+int changeInt(char a){
+	int c;
+	stringstream obj;
+	obj << a;
+	obj >> c;
+
+	return c;
+}
+
+int main(){
+	sumOperation sum;
+	cout<<"Penjumlahan Array\n";
+	cout<<"=================\n";
+	cout<<"Data A: "; cin >> sum.a;
+	cout<<"Data B: "; cin >> sum.b;
+	
+	
+	sum.index = 0;
+	sum.aString = changeStr(sum.a);
+	sum.bString = changeStr(sum.b);
+
+	int lenghtA = sum.aString.length();
+	int lenghtB = sum.bString.length();
+
+	for (int i=0; i<lenghtA; i++) {
+		sum.arrA[i] = changeInt(sum.aString[i]);
+	}
+
+	for (int i=0; i<lenghtB; ++i) {
+		sum.arrB[i] = changeInt(sum.bString[i]);
+	}
+
+	if (lenghtA >= lenghtB) {
+		for (int i = lenghtA-1; i >= 0; i--) {
+			if (lenghtB >= 1){
+				sum.arrC[sum.index] = sum.arrA[i] + sum.arrB[lenghtB-1];
+				lenghtB--;
+				sum.index++;
+			} else {
+				sum.arrC[sum.index] = sum.arrA[i];
+				sum.index++;
+			}
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < sum.index; i++) {
+		if (sum.arrC[i] > 9) {
+			sum.arrC[i+1] = sum.arrC[i+1] + 1;
+			sum.arrC[i] = sum.arrC[i]-10;
+		}
+	}
+
+	cout << "Hasil : ";
+	for (int i = sum.index-1; i>=0; i--) {
+		cout << sum.arrC[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
